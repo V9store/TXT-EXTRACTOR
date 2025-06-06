368939740e12a36c4046b10d4c255e25e3a0cd79
V9store <v9store41@gmail.com>
2025-06-06 13:48:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", ""))  # Replace "123456" with your actual api_id or use .env
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("21283957", ""))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("aade44a828de52da2a6ef816b120020b", "")
+BOT_TOKEN = os.environ.get("7902772572:AAHsBU25TDn-GUGa9MXui0gR2pIw7LNB82I", "")
 
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Your Telegram user ID
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
